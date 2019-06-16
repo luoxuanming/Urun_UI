dcbba60632fa97ef9be8b89ffe0386796012d0d4
luoxuanming <1316570222@qq.com>
2019-06-16 22:03:58 +0800

    悬停下拉菜单

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <router-view/> -->
+    <dropdown @change="aa" :dataList="dataList"></dropdown>
   </div>
 </template>
 
 <script>
+import dropdown from "./base/dropdown/dropdown"
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      dataList:[
+        {name: "选项一2"},
+        {name: "选项二1"}
+      ]
+    }
+  },
+  components:{
+    dropdown
+  },
+  methods:{
+    aa(){
+      console.log()
+    }
+  }
 }
 </script>
 
 <style>
+html,body{
+    padding:0;
+    margin:0;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
